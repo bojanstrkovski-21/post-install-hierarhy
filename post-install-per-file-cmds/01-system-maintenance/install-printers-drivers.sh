07917a17d5515afa2bad7fa0371d939c28d059b0
bojanstrkovski-21 <115637403+bojanstrkovski-21@users.noreply.github.com>
2026-05-13 10:29:36 +0200

    Update printer driver installation script

--- post-install-per-file-cmds/01-system-maintenance/install-printers-drivers.sh
+++ post-install-per-file-cmds/01-system-maintenance/install-printers-drivers.sh
@@ -3,16 +3,33 @@
 sudo pacman -S --noconfirm --needed pacutils
 
 sudo pacinstall --resolve-conflicts=all --no-confirm \
+brscan4 \
+brscan5 \
+brscan-skey \
+capt-src \
+cnijfilter2 \
 cups \
 cups-pdf \
 cups-browsed \
 cups-filters \
+cups-pk-helper \
+epson-inkjet-printer-escpr \
+epson-inkjet-printer-escpr2 \
 bluez-cups \
+foomatic-db \
+foomatic-db-engin \
+foomatic-db-gutenprint-ppds \
 ghostscript \
 gsfonts \
 gutenprint \
-foomatic-db-gutenprint-ppds \
+hplip \
+hplip-plugin \
+python-pillow 
+python-pip 
+python-reportlab
 libcups \
+simple-scan \
+splix \
 system-config-printer
 
 
