7f73d21779fe266b0fa6220540c3bba303f637fc
bojanstrkovski-21 <115637403+bojanstrkovski-21@users.noreply.github.com>
2026-05-13 09:50:02 +0200

    Update add-nemesis-repo.sh to configure nemesis repo
    Removed package installation commands and added nemesis repository to pacman configuration.

--- post-install-per-file-cmds/01-system-maintenance/add-nemesis-repo.sh
+++ post-install-per-file-cmds/01-system-maintenance/add-nemesis-repo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -Syy --needed --noconfirm \
-archlinux-tweak-tool-git \
-arcolinux-app-glade-git \
-sofirem-git
+echo -e "\n[nemesis_repo]\nSigLevel = Never\nServer = https://erikdubois.github.io/\$repo/\$arch" | sudo tee -a /etc/pacman.conf
+# echo -e "\n[arcolinux_repo]\nSigLevel = Never\nServer = https://arcolinux.github.io/\$repo/\$arch" | sudo tee -a /etc/pacman.conf
+# echo -e "\n[arcolinux_repo_3party]\nSigLevel = Never\nServer = https://arcolinux.github.io/\$repo/\$arch" | sudo tee -a /etc/pacman.conf
+sudo pacman -Syy
