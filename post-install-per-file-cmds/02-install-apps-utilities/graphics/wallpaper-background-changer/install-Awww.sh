fd97bf3057b6aadf64e7be90e2440b07b0f7ea0f
bojanstrkovski-21 <115637403+bojanstrkovski-21@users.noreply.github.com>
2026-05-13 13:42:32 +0200

    Update and rename install-swww.sh to install-Awww.sh

---
+++ post-install-per-file-cmds/02-install-apps-utilities/graphics/wallpaper-background-changer/install-Awww.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Installing Awww..."
+sudo pacman -S --needed --noconfirm awww
+echo "Done."
--- post-install-per-file-cmds/02-install-apps-utilities/graphics/wallpaper-background-changer/install-swww.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Installing Swww..."
-sudo pacman -S --needed --noconfirm swww
-echo "Done."
